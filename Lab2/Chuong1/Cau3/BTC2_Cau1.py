a577a4429efa66c1d68516d28bc51a9e782ad75b
Mạnh Hùng <manhhung20050622@gmail.com>
2025-12-22 21:55:37 +0700

    Add files via upload

---
+++ Lab2/Chuong1/Cau3/BTC2_Cau1.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+img = cv2.imread(r"D:\XLH.a\BT_C2_c123\anh_mau.jpg")  
+
+if img is None:
+    print("Không đọc được ảnh!")
+    exit()
+
+value = 50  
+
+bright_img = cv2.add(img, np.ones(img.shape, dtype="uint8") * value)
+
+dark_img = cv2.subtract(img, np.ones(img.shape, dtype="uint8") * value)
+
+cv2.imshow("Anh goc", img)
+cv2.imshow("Tang do sang", bright_img)
+cv2.imshow("Giam do sang", dark_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
