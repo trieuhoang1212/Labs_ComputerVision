b05d3cce8968e996fc5bba1ef47c7c9e4fe0dd65
trieuhoang1212 <trieunh2523@ut.edu.vn>
2025-12-25 11:19:14 +0700

    COMPLETE: cau1,2 chuong2 lab3 và thêm anh_mau

--- Lab3/Chuong2/Cau1/srs/cau1.py
+++ Lab3/Chuong2/Cau1/srs/cau1.py
@@ -1,1 +1,18 @@
-# THỰC HIỆN THUẬT TOÁN CANNY BẰNG CÁC THƯ VIỆN:
+# sử dụng thuật toán cv2.canny để phát hiện cạnh trong hình ảnh
+import cv2 
+from matplotlib import pyplot as plt
+
+img = cv2.imread('Lab3/assets/images/anh_mau1.jpg', cv2.IMREAD_GRAYSCALE)
+
+# ngưỡng phổ biến hay sử dụng nhất và giúp ảnh rõ nét.
+t_lower = 50;
+t_upper = 150;
+
+# Giải thích: img là ảnh đầu vào, t_lower và t_upper là hai ngưỡng để phát hiện cạnh
+edges = cv2.Canny(img, t_lower, t_upper,apertureSize=3, L2gradient=True)
+cv2.imwrite('Lab3/Chuong2/Cau1/input_cau1.jpg', edges)
+plt.subplot(121), plt.imshow(img, cmap='gray')
+plt.title('Ảnh gốc')
+plt.subplot(122), plt.imshow(edges, cmap='gray')
+plt.title('Ảnh cạnh')
+plt.show()
