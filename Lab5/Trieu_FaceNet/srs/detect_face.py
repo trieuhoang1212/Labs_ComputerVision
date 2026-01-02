b79ac2e013e061510cfa9aa3a65720a85754564b
trieuhoang1212 <trieunh2523@ut.edu.vn>
2026-01-02 10:52:00 +0700

    CREATE: hoàn thành các basic về capture và dectect

---
+++ Lab5/Trieu_FaceNet/srs/detect_face.py
@@ -0,0 +1,27 @@
+import cv2
+from facenet_pytorch import MTCNN
+import torch
+
+device =  torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(device)
+mtcnn = MTCNN(thresholds= [0.7, 0.7, 0.8] ,keep_all=True, device = device)
+cap = cv2.VideoCapture(0)
+# Set video frame width and height
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+
+# Read video frame-by-frame
+while cap.isOpened():
+    isSuccess, frame = cap.read()
+    if isSuccess:
+        boxes, _ = mtcnn.detect(frame)
+        if boxes is not None:
+            for box in boxes:
+                bbox = list(map(int,box.tolist()))
+                frame = cv2.rectangle(frame,(bbox[0],bbox[1]),(bbox[2],bbox[3]),(0,0,225),3)
+    cv2.imshow('Face detection', frame)
+    if cv2.waitKey(1)&0xFF == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
+
